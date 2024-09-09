bf93411d0565d4f685f64259efaef4631feb0aa5
Erin Hurm <ehurm@ivytech.edu>
2024-09-08 20:59:25 -0500

    seems to work??

--- M3_lab.py
+++ M3_lab.py
@@ -4,17 +4,7 @@ class Vehicle():
         
 
 class Automobile(Vehicle):
-    def __init__(self, vehicleType, year, make, model, doors, roof):
-        valid_doors = ["2", "4"]
-        valid_roof = ["solid", "sun roof"]
-        
-        year = input("Year: ")
-        make = input("Make: ")
-        model = input("Model: ")
-        doors = self.error_check(input("Doors (2 or 4): "), valid_doors)
-        roof = self.error_check(input("Roof (solid or sun roof):"),)
-        
-        
+    def __init__(self, vehicleType, year, make, model, doors, roof): 
         super().__init__(vehicleType)
         self.year = year
         self.make = make
@@ -23,15 +13,38 @@ class Automobile(Vehicle):
         self.roof = roof
 
     def __str__(self):
-        return f"Vehicle type: {self.vehicleType}\nYear: {self.year}\nMake: {self.make}\n"
+        return f"Vehicle type: {self.vehicleType}\nYear: {self.year}\nMake: {self.make}\nModel: {self.model}\nDoors: {self.doors}\nRoof: {self.roof}"
 
-    def error_check(self, trait, check):
-        if self.trait not in valid_doors
-
+## App start
+def error_check(trait, check):
+    if trait in check:
+        return True
+    else:
+        return False
 
 vehicleType = "car"
 
 if vehicleType == "car":
-    myVehicle = Automobile(vehicleType)
 
-myVehicle()
+    #for error checking
+    valid_doors = ["2", "4"]
+    valid_roof = ["solid", "sun roof"]
+
+    doors = ""
+    roof = ""
+        
+    year = input("Year: ")
+    make = input("Make: ")
+    model = input("Model: ")
+    doors = input("Doors (2 or 4): ")
+    # check for validity
+    while not error_check(doors, valid_doors):
+        doors = input("Doors (2 or 4): ")
+        
+    roof = input("Roof (solid or sun roof): ")
+    while not error_check(roof, valid_roof):
+        roof = input("Roof (solid or sun roof): ")
+        
+    myVehicle = Automobile(vehicleType, year, make, model, doors, roof)
+    
+print(myVehicle)
